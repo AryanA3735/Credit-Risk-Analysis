aa5a8baf642ac4ffa170a9f8e4e47a2ef0026735
Aryan Ali <aliaryan3735@gmail.com>
2023-09-19 22:28:23 +0530

    updating file

---
+++ src/front_end/app.py
@@ -0,0 +1,30 @@
+# Import Libraries
+import json
+import requests
+import streamlit as st
+
+# Initialization
+API_URL = 'http://127.0.0.1:8000'
+with open('D:/Career/Project/Credit Risk Analysis/src/front_end/config/input_options.json', 'r') as file:
+    input_options = json.load(file)
+
+# Main UI
+st.title("IBRD Credit Scorecard Predictive Engine")
+
+# Service Inputs
+region = st.selectbox("Region", input_options["regions"])
+country = st.selectbox("Country", input_options["countries"])
+guarantor = st.selectbox("Guarantor", input_options["guarantors"])
+loan_type = st.selectbox("Loan Type", input_options["loan_types"])
+principal_amount = st.number_input("Principal Amount", min_value=0)
+
+# Predict Score Button
+if st.button("Predict Score"):
+    data = {"region": region,
+            "country": country,
+            "guarantor": guarantor,
+            "loan_type": loan_type,
+            "principal_amount": principal_amount}
+    response = requests.post(API_URL, json={"data": data})
+    prediction = response.json()["score"]
+    st.write(f"Predicted Score: {prediction}")
